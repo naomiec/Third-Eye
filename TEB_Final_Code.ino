7c92a5e8a83ea6b1b33d7818c49accf47e87c1de
naomiec <31399879+naomiec@users.noreply.github.com>
2018-07-16 13:31:45 -0700

    Add files via upload

--- TEB_Final_Code.ino
+++ TEB_Final_Code.ino
@@ -1,52 +1,74 @@
-
-  const int pingTrigPin = 12; //Trigger connected to D12
-  const int pingEchoPin = 11; //Echo connected to D11
-  int buz = 4 ; //Buzzer connected to D4
-  int led = 2; //Led connected to D2
-  int vibr = 6; //Vibrating Motor connected to D6
-  void setup() {
-    Serial.begin(9600);
-    pinMode(buz, OUTPUT);
-    pinMode(led, OUTPUT);
-    pinMode(vibr, OUTPUT);
-  }
-  void loop()
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
+#include <TinyGPS++.h>
+//GPS pins and baud
+static const int RXPin = 4, TXPin = 3;
+static const uint32_t GPSBaud = 9600;
+TinyGPSPlus gps;
+SoftwareSerial ss(RXPin, TXPin);
+//Back to main code
+const int pingTrigPin = 12; //Trigger connected to D12
+const int pingEchoPin = 11; //Echo connected to D11
+int buz = 4 ; //Buzzer connected to D4
+int vibr = 6; //Vibrating Motor connected to D6
+void setup() {
+  // Initialize serial output.
+  Serial.begin(9600);
+  Serial.println("Tracking...");
+  //Back to main code and GPS setup
+  pinMode(buz, OUTPUT);
+  pinMode(vibr, OUTPUT);
+  //ss.begin(GPSBaud);
+}
+void loop()
+{
+  long duration, cm;
+  pinMode(pingTrigPin, OUTPUT);
+  digitalWrite(pingTrigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingTrigPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingTrigPin, LOW);
+  pinMode(pingEchoPin, INPUT);
+  duration = pulseIn(pingEchoPin, HIGH);
+  cm = microsecondsToCentimeters(duration);
+  if (cm <= 78 && cm > 0)
   {
-    long duration, cm;
-    pinMode(pingTrigPin, OUTPUT);
-    digitalWrite(pingTrigPin, LOW);
-    delayMicroseconds(2);
-    digitalWrite(pingTrigPin, HIGH);
-    delayMicroseconds(5);
-    digitalWrite(pingTrigPin, LOW);
-    pinMode(pingEchoPin, INPUT);
-    duration = pulseIn(pingEchoPin, HIGH);
-    cm = microsecondsToCentimeters(duration);
-     if(cm<=150 && cm>0)
-     {
-      int d= map(cm, 1, 100, 20, 2000);
-      digitalWrite(led, HIGH);
-      delay(100);
-      delay(d);
-      digitalWrite(buz, HIGH);
-      delay(300);
-      digitalWrite(buz,LOW);
-      digitalWrite(vibr,HIGH);
-      delay(300);
-      digitalWrite(vibr,LOW);
-    } else {
-      digitalWrite(led, HIGH);
-      delay(100);
-      digitalWrite(led, LOW);
-      delay(100);
-    }
-    Serial.print(cm);
-    Serial.print("cm");
-    Serial.println();
-    delay(100);
-    }
+    int d = map(cm, 1, 100, 20, 2000);
+    digitalWrite(buz, HIGH);
+    delay(200);
+    digitalWrite(buz, LOW);
+    digitalWrite(vibr, HIGH);
+    delay(d);
+    digitalWrite(vibr, LOW);
+    delay(300);
+  } else if (cm <= 200 && cm > 78) {
+    int d = map(cm, 1, 100, 20, 2000);
+    Serial.print("Distance: ");
+    digitalWrite(vibr, HIGH);
+    delay(d);
+    digitalWrite(vibr, LOW);
+  }
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+}
 
- long microsecondsToCentimeters(long microseconds)
- {
- return microseconds /29/2;
- }
+long microsecondsToCentimeters(long microseconds)
+{
+  return microseconds / 29 / 2;
+}
+void displayInfo()
+{
+  if (gps.location.isValid())
+  {
+    Serial.print("Lat=");
+    Serial.print(gps.location.lat(), 6);
+    Serial.print("Lng=");
+    Serial.print(gps.location.lng(), 6);
+  }
+  else
+  {
+    Serial.print("No Connection");
+  }
+}
